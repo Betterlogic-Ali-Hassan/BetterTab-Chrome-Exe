f4b3a7b7064007056156d42393391db391de6094
HassanThePassionate <maqsoodia777@gmail.com>
2025-04-07 19:36:07 +0500

    Update

--- src/components/gridLayout/GridStackBtns.tsx
+++ src/components/gridLayout/GridStackBtns.tsx
@@ -34,7 +34,7 @@ const GridStackBtns = ({ grid }: Props) => {
 
     if (width === 1 && height === 1) {
       element.innerHTML = `
-        <div class="flex items-center flex-col justify-center gap-2 grid-stack-item-content  ">
+        <div class="flex items-center flex-col justify-center gap-2 grid-stack-item-content bg-card rounded-[10px] py-3 ">
           <div class="min-[1600px]:w-12 min-[1600px]:h-12 w-11 h-11 flex items-center justify-center rounded-full bg-green-200">
 				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 11 12" fill="none">
 					<path d="M1.375 11.5C2.13125 11.5 2.75 10.8813 2.75 10.125V5.3125C2.75 4.55625 2.13125 3.9375 1.375 3.9375C0.61875 3.9375 0 4.55625 0 5.3125V10.125C0 10.8813 0.61875 11.5 1.375 11.5ZM8.25 8.0625V10.125C8.25 10.8813 8.86875 11.5 9.625 11.5C10.3813 11.5 11 10.8813 11 10.125V8.0625C11 7.30625 10.3813 6.6875 9.625 6.6875C8.86875 6.6875 8.25 7.30625 8.25 8.0625ZM5.5 11.5C6.25625 11.5 6.875 10.8813 6.875 10.125V1.875C6.875 1.11875 6.25625 0.5 5.5 0.5C4.74375 0.5 4.125 1.11875 4.125 1.875V10.125C4.125 10.8813 4.74375 11.5 5.5 11.5Z" fill="#2A9D8F"></path>
@@ -45,9 +45,7 @@ const GridStackBtns = ({ grid }: Props) => {
       `;
     } else if (width === 2 && height === 2) {
       element.innerHTML = `
-        <div class="grid-stack-item-content !px-3 gap-2 ">
-        <div>
-        </div>
+        <div class="grid-stack-item-content !px-3 gap-2 bg-card rounded-[10px] py-3">
           <div class="absolute top-0 left-0 w-20 h-20 bg-gradient-to-br dark:from-purple-500/90 from-purple-500/60 to-[#dadada] dark:to-[#181D21] rounded-full flex items-center justify-center blur-2xl"></div>
         <div class="relative flex flex-col gap-2">
 			<div class="flex items-center justify-center w-10 h-10 rounded-full bg-gradient-to-br from-purple-500 to-blue-500 text-white">
